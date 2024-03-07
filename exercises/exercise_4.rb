a1f4641f9fb885a35c3b60c46fcb9b5f28af581f
Paul Formby <paulformby@icloud.com>
2024-03-07 18:40:18 +0000

    ex 4 and 5 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,37 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+mission_store = Store.create(
+  name: "Mission", 
+  annual_revenue: 224000, 
+  mens_apparel: false, 
+  womens_apparel: true
+  )
+whistler_store = Store.create(
+  name: "Whistler", 
+  annual_revenue: 1900000 , 
+  mens_apparel: true, 
+  womens_apparel: false
+  )
+  yaletown_store = Store.create(
+  name: "Yaletown", 
+  annual_revenue: 430000, 
+  mens_apparel: true, 
+  womens_apparel: true
+  )
+
+  @mens_stores = Store.where( 
+    mens_apparel: true
+    )
+
+    @mens_stores.each do |store|
+      puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+    end
+
+    @womens_stores = Store.where(
+      womens_apparel: true
+      ).where('annual_revenue < ?', 1_000_000)
+
+      @womens_stores.each do |store|
+        puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+      end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+store_count = Store.count
+puts "Total Annual Revenue of all stores: #{total_revenue}"
+puts "Average Annual Revenue per store: #{total_revenue / store_count}"
+
+stores_1m_count = Store.where(
+  'annual_revenue > ?', 1_000_000
+  )
+  puts stores_1m_count.count
