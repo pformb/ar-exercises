a087d0a3295427ca73b66856abe39eea15e06b01
Paul Formby <paulformby@icloud.com>
2024-03-09 03:07:35 +0000

    ex 6 and 7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,49 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+# Define the Employee class
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60
+  )
+
+  @store1.employees.create(
+  first_name: "Paul", 
+  last_name: "Formby", 
+  hourly_rate: 100
+  )
+
+  @store1.employees.create(
+  first_name: "James", 
+  last_name: "Miller", 
+  hourly_rate: 75
+  )
+
+  @store2.employees.create(
+  first_name: "Homer", 
+  last_name: "Simpson", 
+  hourly_rate: 45
+  )
+
+  @store2.employees.create(
+    first_name: "Marge", 
+    last_name: "Thompson", 
+    hourly_rate: 25
+    )
+
+    @store2.employees.create(
+      first_name: "Liah", 
+      last_name: "Kisar", 
+      hourly_rate: 80
+      )
+
+      puts @store1.employees
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,33 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+  private
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry at least one of men's or women's apparel")
+    end
+  end
+end
